d15b694a57b1b30792ae591449ab5101b7f2e767
Muhammad Aulia Rahman, S.Kom. <mauliarahman214@gmail.com>
2024-06-30 11:38:06 +0700

    initial

---
+++ src/app/components/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+
+    return (
+        <section className="lg:py-16" >
+
+
+        </section >
+    );
+};
+
+export default About
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+const plugin = require('tailwindcss/plugin')
+import { PluginAPI } from 'tailwindcss/types/config';
 
 const config: Config = {
   content: [
@@ -13,8 +15,26 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      textShadow: {
+        sm: '1px 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '2px 2px 4px var(--tw-shadow-color)',
+        lg: '4px 4px 8px var(--tw-shadow-color)',
+        xl: '4px 4px 16px var(--tw-shadow-color)',
+      }
     },
   },
-  plugins: [],
+  plugins: [plugin(function ({ matchUtilities, theme }: PluginAPI) {
+    matchUtilities(
+      {
+        'text-shadow': (value: string) => ({
+          textShadow: value,
+        }),
+      },
+      { values: theme('textShadow') }
+    )
+  })
+  ]
 };
 export default config;
+
+
